565ecf14b801166b2359b1d3a269cc6900eca7c8
kelwinv <kelwin08@gmail.com>
2021-05-25 15:27:43 -0300

    podcastr v1.0 nlw05

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "../styles/global.scss";
+
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+import { PlayerContextProvider } from "../context/PlayerContext";
+
+import styles from "../styles/app.module.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  );
+}
+
+export default MyApp;
